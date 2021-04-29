65752a269704cbb09c18f100b153f2af9011638b
phoenixtahoe <phoenix@pdavid.net>
2021-04-29 00:01:45 -0700

    Done

---
+++ app.py
@@ -0,0 +1,63 @@
+"""Blogly application."""
+
+from flask import Flask, request, redirect, render_template
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///Work'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def landingPage():
+    return redirect("/users")
+
+@app.route('/users')
+def userPage():
+    users = User.query.order_by(User.last_name, User.first_name).all()
+    return render_template('users/index.html', users=users)
+
+@app.route('/users/new', methods=["GET"])
+def renderForm():
+    return render_template('users/form.html')
+
+@app.route('/users/new', methods=["POST"])
+def createUser():
+    user = User(first_name=request.form['first_name'], last_name=request.form['last_name'], image_url=request.form['image_url'])
+    db.session.add(user)
+    db.session.commit()
+    return redirect("/users")
+
+@app.route('/users/<int:user_id>')
+def users_show(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('users/show.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def users_edit(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('users/edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=["POST"])
+def users_update(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.add(user)
+    db.session.commit()
+
+    return redirect("/users")
+
+@app.route('/users/<int:user_id>/delete', methods=["POST"])
+def users_destroy(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+
+    return redirect("/users")
+
---
+++ models.py
@@ -0,0 +1,17 @@
+"""Models for Blogly."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False)
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
